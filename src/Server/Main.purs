abcf64d3fd4ed8f3ea3d6b27646407ff24ddcfcb
bouzuya <m@bouzuya.net>
2018-05-06 21:35:33 +0900

    add dummy code

---
+++ src/Server/Main.purs
@@ -0,0 +1,9 @@
+module Server.Main (main) where
+
+import Prelude (Unit)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
+
+main :: forall e. Eff (console :: CONSOLE | e) Unit
+main = do
+  log "Hello"
