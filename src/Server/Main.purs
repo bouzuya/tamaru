9e7078f239d59982b33d6167d1bda4c3f4c2e64a
bouzuya <m@bouzuya.net>
2018-05-08 08:49:06 +0900

    extract Server.Node.Server

--- src/Server/Main.purs
+++ src/Server/Main.purs
@@ -2,33 +2,9 @@ module Server.Main (main) where
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.Maybe (Maybe(..))
-import Node.Encoding as Encoding
-import Node.HTTP (HTTP)
-import Node.HTTP as HTTP
-import Node.Stream as Stream
-import Prelude (Unit, bind, pure, unit, ($))
-
-handleRequest
-  :: forall e
-  . HTTP.Request
-  -> HTTP.Response
-  -> Eff (console :: CONSOLE, http :: HTTP | e) Unit
-handleRequest _request response = do
-  _ <- HTTP.setStatusCode response 200
-  _ <- HTTP.setHeader response "Content-Type" "text/plain"
-  let
-    writable = HTTP.responseAsStream response
-  _ <- Stream.writeString writable Encoding.UTF8 "OK" $ pure unit
-  Stream.end writable $ pure unit
+import Prelude (Unit)
+import Server.Node.Server (HTTP, run)
 
 main :: forall e. Eff (console :: CONSOLE, http :: HTTP | e) Unit
 main = do
-  server <- HTTP.createServer handleRequest
-  let
-    listenOptions =
-      { hostname: "0.0.0.0"
-      , port: 3000
-      , backlog: Nothing
-      }
-  HTTP.listen server listenOptions (log "listening...")
+  run (log "listening...")
---
+++ src/Server/Node/Server.purs
@@ -0,0 +1,67 @@
+module Server.Node.Server
+  ( module HTTP
+  , run
+  ) where
+
+import Control.Monad.Eff (Eff)
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+import Node.Encoding as Encoding
+import Node.HTTP (HTTP)
+import Node.HTTP as HTTP
+import Node.Stream as Stream
+import Prelude (Unit, bind, pure, unit, ($))
+
+type Body = String
+type Header = Tuple String String
+newtype StatusCode = StatusCode Int
+type Response = { body :: Body, header :: Header, status :: StatusCode }
+
+setBody
+  :: forall e. HTTP.Response -> Body -> Eff (http :: HTTP | e) Unit
+setBody response body = do
+  let writable = HTTP.responseAsStream response
+  _ <- Stream.writeString writable Encoding.UTF8 body $ pure unit
+  Stream.end writable $ pure unit
+
+setHeader
+  :: forall e. HTTP.Response -> Header -> Eff (http :: HTTP | e) Unit
+setHeader response (Tuple name value) =
+  HTTP.setHeader response name value
+
+setStatusCode
+  :: forall e. HTTP.Response -> StatusCode -> Eff (http :: HTTP | e) Unit
+setStatusCode response (StatusCode code) =
+  HTTP.setStatusCode response code
+
+writeResponse
+  :: forall e. HTTP.Response -> Response -> Eff (http :: HTTP | e) Unit
+writeResponse response { body, header, status } = do
+  _ <- setStatusCode response status
+  _ <- setHeader response header
+  setBody response body
+
+handleRequest
+  :: forall e
+  . HTTP.Request
+  -> HTTP.Response
+  -> Eff (http :: HTTP | e) Unit
+handleRequest _request response = do
+  let
+    myResponse =
+      { body: "OK"
+      , header: (Tuple "Content-Type" "text/plain")
+      , status: (StatusCode 200)
+      }
+  writeResponse response myResponse
+
+run :: forall e. Eff (http :: HTTP | e) Unit -> Eff (http :: HTTP | e) Unit
+run f = do
+  server <- HTTP.createServer handleRequest
+  let
+    listenOptions =
+      { hostname: "0.0.0.0"
+      , port: 3000
+      , backlog: Nothing
+      }
+  HTTP.listen server listenOptions f
