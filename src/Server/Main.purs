ff9d7e0fb5cc8cec7d1cc735abc0a7d0227a61a7
bouzuya <m@bouzuya.net>
2018-05-13 18:48:08 +0900

    add normalizePath

--- src/Server/Main.purs
+++ src/Server/Main.purs
@@ -5,6 +5,7 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Maybe.Trans (MaybeT(..), runMaybeT)
 import Data.Array as Array
+import Data.Either (Either(..))
 import Data.Foldable (intercalate)
 import Data.Int as Int
 import Data.Maybe (Maybe(..), fromMaybe)
@@ -12,39 +13,61 @@ import Data.String (Pattern(..))
 import Data.String as String
 import Data.Tuple (Tuple(..))
 import Node.Process (PROCESS, lookupEnv)
-import Prelude (Unit, bind, not, pure, show, ($), (<$>), (<<<), (<>))
-import Server.HTTP.StatusCode (status200, status404)
+import Prelude (Unit, bind, not, pure, show, ($), (<$>), (<<<), (<>), (==))
+import Server.HTTP.StatusCode (status200, status302, status404)
 import Server.Node.Server (Request, Response, ServerEff, run)
 import Server.Route (route)
+
+joinPath :: Array String -> String
+joinPath pathPieces = "/" <> (intercalate "/" pathPieces)
+
+splitPath :: String -> Array String
+splitPath path =
+  Array.filter (not <<< String.null) $ String.split (Pattern "/") path
+
+parsePath :: String -> Either String (Array String)
+parsePath pathname =
+  let
+    splittedPath = splitPath pathname
+    normalizedPath = joinPath splittedPath
+  in
+    if pathname == normalizedPath
+    then Right splittedPath
+    else Left normalizedPath
 
 onRequest
   :: forall e
   . Request
   -> Aff (ServerEff e) Response
 onRequest { body, headers, method, pathname, searchParams } = do
-  let
-    pathSegments = String.split (Pattern "/") pathname
-    normalizedPath = Array.filter (not <<< String.null) pathSegments
-  case route method normalizedPath of
-    Just action ->
-      pure
-        { body:
-            intercalate ", "
-              [ "method: " <> show method
-              , "pathname: " <> pathname
-              , "query: " <> (intercalate "," (show <$> searchParams))
-              , "body: " <> body
-              , "action: " <> show action
-              ]
-        , headers: [(Tuple "Content-Type" "text/plain"), (Tuple "X-Foo" "bar")]
-        , status: status200
-        }
-    Nothing ->
+  case parsePath pathname of
+    Left location ->
       pure
         { body: ""
-        , headers: [(Tuple "Content-Type" "text/plain")]
-        , status: status404
+        , headers: [Tuple "Location" location]
+        , status: status302
         }
+    Right normalizedPath ->
+    case route method normalizedPath of
+      Nothing ->
+        pure
+          { body: ""
+          , headers: [(Tuple "Content-Type" "text/plain")]
+          , status: status404
+          }
+      Just action ->
+        pure
+          { body:
+              intercalate ", "
+                [ "method: " <> show method
+                , "pathname: " <> pathname
+                , "query: " <> (intercalate "," (show <$> searchParams))
+                , "body: " <> body
+                , "action: " <> show action
+                ]
+          , headers: [(Tuple "Content-Type" "text/plain"), (Tuple "X-Foo" "bar")]
+          , status: status200
+          }
 
 onListen :: forall e. Eff (console :: CONSOLE | e) Unit
 onListen = log "listening..."
