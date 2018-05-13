4e9dfa50f3a85f533132bd4e0adf235e30c3acdb
bouzuya <m@bouzuya.net>
2018-05-13 18:47:54 +0900

    add some routings

--- src/Server/Route.purs
+++ src/Server/Route.purs
@@ -1,5 +1,7 @@
 module Server.Route
   ( Action(..)
+  , GroupId
+  , DataId
   , route
   ) where
 
@@ -8,12 +10,29 @@ import Prelude
 import Bouzuya.HTTP.Method (Method(..))
 import Data.Maybe (Maybe(..))
 
+type GroupId = String
+type DataId = String
+
 data Action
   = GetIndex
+  | GetGroupList
+  | GetGroup GroupId
+  | GetGroupDataList GroupId
+  | GetGroupData GroupId DataId
 
 instance showAction :: Show Action where
   show GetIndex = "GetIndex"
+  show GetGroupList = "GetGroupList"
+  show (GetGroup groupId) = "GetGroup(" <> groupId <> ")"
+  show (GetGroupDataList groupId) = "GetGroupDataList(" <> groupId <> ")"
+  show (GetGroupData groupId dataId)
+    = "GetGroupData(" <> groupId <> "," <> dataId <> ")"
 
 route :: Method -> Array String -> Maybe Action
 route GET [] = Just GetIndex
+route GET ["groups"] = Just GetGroupList
+route GET ["groups", groupId] = Just (GetGroup groupId)
+route GET ["groups", groupId, "data"] = Just (GetGroupDataList groupId)
+route GET ["groups", groupId, "data", dataId]
+  = Just (GetGroupData groupId dataId)
 route _ _ = Nothing
