fb2637ca1a7e00d5725e617e181ca6c0a4a96f82
bouzuya <m@bouzuya.net>
2018-05-25 23:05:57 +0900

    add some helpers

--- src/Server/Response.purs
+++ src/Server/Response.purs
@@ -6,43 +6,54 @@ module Server.Response
   , response500
   ) where
 
+import Bouzuya.HTTP.Header (Header)
 import Bouzuya.HTTP.Response (Response)
 import Bouzuya.HTTP.StatusCode (status200, status302, status400, status404, status500)
 import Data.Show (show)
 import Data.Tuple (Tuple(..))
 import Server.View (View)
 
+-- Header helpers
+
+contentType :: String -> Header
+contentType = Tuple "Content-Type"
+
+location :: String -> Header
+location = Tuple "Location"
+
+-- Response helpers
+
 response200 :: View -> Response
 response200 view =
   { body: show view
-  , headers: [(Tuple "Content-Type" "text/plain")]
+  , headers: [contentType "text/plain"]
   , status: status200
   }
 
 response302 :: String -> Response
-response302 location =
+response302 loc =
   { body: ""
-  , headers: [Tuple "Location" location]
+  , headers: [location loc]
   , status: status302
   }
 
 response400 :: Response
 response400 =
   { body: ""
-  , headers: [(Tuple "Content-Type" "text/plain")]
+  , headers: [contentType "text/plain"]
   , status: status400
   }
 
 response404 :: Response
 response404 =
   { body: ""
-  , headers: [(Tuple "Content-Type" "text/plain")]
+  , headers: [contentType "text/plain"]
   , status: status404
   }
 
 response500 :: Response
 response500 =
   { body: ""
-  , headers: [(Tuple "Content-Type" "text/plain")]
+  , headers: [contentType "text/plain"]
   , status: status500
   }
