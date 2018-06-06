80e43244641888f16094ce7d5a53465002af0835
bouzuya <m@bouzuya.net>
2018-06-06 21:59:12 +0900

    extract loadConfig

--- src/Server/Main.purs
+++ src/Server/Main.purs
@@ -18,7 +18,7 @@ import Data.String as String
 import Node.Process (PROCESS, lookupEnv)
 import Prelude (Unit, bind, pure, ($), (<$>))
 import Server.Action (handleAction)
-import Server.DB (Context)
+import Server.DB (Context, Config)
 import Server.Path (parsePath')
 import Server.Response (response302, response404)
 import Server.Route (route)
@@ -43,6 +43,15 @@ onRequest context request@{ method, pathname } = do
 onListen :: forall e. Eff (console :: CONSOLE | e) Unit
 onListen = log "listening..."
 
+loadConfig :: forall e. Eff (process :: PROCESS | e) (Maybe Config)
+loadConfig = runMaybeT do
+  googleApiClientEmail <- MaybeT $ lookupEnv "TAMARU_GOOGLE_API_CLIENT_EMAIL"
+  googleApiPrivateKey' <- MaybeT $ lookupEnv "TAMARU_GOOGLE_API_PRIVATE_KEY"
+  spreadsheetId <- MaybeT $ lookupEnv "TAMARU_SPREADSHEET_ID"
+  googleApiPrivateKey <- pure $
+    String.replaceAll (Pattern "\\n") (Replacement "\n") googleApiPrivateKey'
+  pure { googleApiClientEmail, googleApiPrivateKey, spreadsheetId }
+
 main
   :: forall e
   . Eff
@@ -55,13 +64,7 @@ main
     )
     Unit
 main = launchAff_ do
-  configMaybe <- liftEff $ runMaybeT do
-    googleApiClientEmail <- MaybeT $ lookupEnv "TAMARU_GOOGLE_API_CLIENT_EMAIL"
-    googleApiPrivateKey' <- MaybeT $ lookupEnv "TAMARU_GOOGLE_API_PRIVATE_KEY"
-    spreadsheetId <- MaybeT $ lookupEnv "TAMARU_SPREADSHEET_ID"
-    googleApiPrivateKey <- pure $
-      String.replaceAll (Pattern "\\n") (Replacement "\n") googleApiPrivateKey'
-    pure { googleApiClientEmail, googleApiPrivateKey, spreadsheetId }
+  configMaybe <- liftEff loadConfig
   config <- liftEff $ maybe (throw "INVALID ENV") pure configMaybe
   db <-
     getGroupList
