6d95649f12395660349b3758a0c1ab0b082eb9a5
bouzuya <m@bouzuya.net>
2018-06-15 23:03:35 +0900

    replace ssr element

--- src/Client/Main.purs
+++ src/Client/Main.purs
@@ -3,8 +3,11 @@ module Client.Main (main) where
 import Client.Component.Body as Body
 import Control.Bind (bind, pure)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Exception (error)
 import Control.Monad.Error.Class (throwError)
+import DOM.HTML.Types (htmlElementToNode)
+import DOM.Node.Node (firstChild, removeChild)
 import DOM.Node.ParentNode (QuerySelector(..))
 import Data.Maybe (maybe)
 import Data.Unit (Unit)
@@ -17,4 +20,7 @@ main = HA.runHalogenAff do
   HA.awaitLoad
   body' <- HA.selectElement (QuerySelector ".body")
   body <- maybe (throwError (error "Could not find .body")) pure body'
+  child' <- liftEff (firstChild (htmlElementToNode body))
+  child <- maybe (throwError (error "no child")) pure child'
+  _ <- liftEff (removeChild child (htmlElementToNode body))
   runUI Body.body { groupList: [] } body
