a71ab7b678e0245f31baad27077a38bc8c7105eb
bouzuya <m@bouzuya.net>
2018-06-20 23:16:49 +0900

    add data input component

---
+++ src/Client/Component/DataInput.purs
@@ -0,0 +1,64 @@
+module Client.Component.DataInput
+  ( Input
+  , Output
+  , Query
+  , dataInput
+  ) where
+
+import Control.Monad.Aff (Aff)
+import DOM (DOM)
+import DOM.Event.Event (Event, preventDefault)
+import Data.Maybe (Maybe(..))
+import Halogen (ClassName(..), liftEff)
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import Prelude (type (~>), Unit, bind, const, pure)
+
+type State = { output :: String, value :: String } -- output for debug
+data Query a
+  = Change String a
+  | Submit Event a
+type Input = Unit
+data Output = Void
+
+dataInput :: forall e. H.Component HH.HTML Query Input Output (Aff (dom :: DOM | e))
+dataInput =
+  H.component
+    { initialState: (const { output: "", value: "" })
+    , render
+    , eval
+    , receiver: const Nothing
+    }
+  where
+  eval :: Query ~> H.ComponentDSL State Query Output (Aff (dom :: DOM | e))
+  eval (Change value next) = do
+    _ <- H.modify (_ { value = value })
+    pure next
+  eval (Submit event q) = do
+    _ <- liftEff (preventDefault event)
+    -- TODO
+    pure q
+
+  render :: State -> H.ComponentHTML Query
+  render state =
+    HH.div
+    [ HP.classes [ ClassName "data-input" ]
+    ]
+    [ HH.form [ HE.onSubmit (HE.input Submit) ]
+      [ HH.label [ HP.classes [ ClassName "value" ] ]
+        [ HH.span [ HP.classes [ ClassName "label" ] ]
+          [ HH.text "value" ]
+        , HH.span [ HP.classes [ ClassName "value" ] ]
+          [ HH.input
+            [ HE.onValueChange (HE.input Change)
+            , HP.name "value"
+            , HP.value state.value
+            ]
+          ]
+        ]
+      , HH.button [ HP.type_ HP.ButtonSubmit ] [ HH.text "OK" ]
+      ]
+    , HH.span [ HP.classes [ ClassName "output" ] ] [ HH.text state.output ]
+    ]
