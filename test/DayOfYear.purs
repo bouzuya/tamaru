70ca348513a27dfc10f06c1ba218cb5d75c329af
bouzuya <m@bouzuya.net>
2018-06-26 23:33:46 +0900

    use Bouzuya.DateTime

--- test/DayOfYear.purs
+++ test/DayOfYear.purs
@@ -1,7 +1,7 @@
 module Test.DayOfYear (tests) where
 
+import Bouzuya.DateTime (Month(..), canonicalDate)
 import Bouzuya.DateTime.Component.DayOfYear (canonicalDateFromDayOfYear, dayOfYear, exactDateFromDayOfYear, lastDayOfYear)
-import Data.Date (Month(..), canonicalDate)
 import Data.Enum (toEnum)
 import Data.Maybe (Maybe(..), fromJust)
 import Partial.Unsafe (unsafePartial)
