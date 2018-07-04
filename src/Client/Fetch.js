2789cab5d910c5ece067a8f6f03140a71ac4262e
bouzuya <m@bouzuya.net>
2018-07-04 23:21:08 +0900

    fix fetch

--- src/Client/Fetch.js
+++ src/Client/Fetch.js
@@ -1,9 +1,21 @@
 "use strict";
 
 exports.fetchImpl = function (options) {
-  return window.fetch(options.url, {
-    body: options.body,
-    headers: options.headers,
-    method: options.method
-  });
+  return function () {
+    return window.fetch(options.url, {
+      body: options.body,
+      headers: options.headers,
+      method: options.method
+    });
+  };
+};
+
+exports.jsonImpl = function (response) {
+  return function () {
+    return response.json();
+  };
+};
+
+exports.statusImpl = function (response) {
+  return response.status;
 };
--- src/Client/Fetch.purs
+++ src/Client/Fetch.purs
@@ -4,26 +4,50 @@ module Client.Fetch
   , fetch
   ) where
 
+import Client.Fetch.Options (FetchOptions, defaults)
 import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff, kind Effect)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Promise (Promise)
 import Control.Promise as Promise
 import Data.Foreign (Foreign)
+import Data.Maybe (Maybe(..))
 import Data.Options (Options, options)
-import Fetch.Options (FetchOptions, defaults)
-import Prelude (append, bind)
+import Prelude (append, bind, eq, pure)
 
 type Effect e = (http :: HTTP | e)
 
 foreign import data HTTP :: Effect
 
+data FetchResponse
+
 foreign import fetchImpl
   :: forall e
   . Foreign
+  -> Eff (Effect e) (Promise FetchResponse)
+
+foreign import jsonImpl
+  :: forall e
+  . FetchResponse
   -> Eff (Effect e) (Promise Foreign)
 
-fetch :: forall e. Options FetchOptions -> Aff (Effect e) Foreign
-fetch opts = do
+foreign import statusImpl :: FetchResponse -> Int
+
+fetch' :: forall e. Options FetchOptions -> Aff (Effect e) FetchResponse
+fetch' opts = do
   promise <- liftEff (fetchImpl (options (append defaults opts)))
   Promise.toAff promise
+
+fetch
+  :: forall e
+  . Options FetchOptions
+  -> Aff (Effect e) { body :: Maybe Foreign, status :: Int }
+fetch opts = do
+  response <- fetch' opts
+  let status = statusImpl response
+  if eq status 204
+    then pure { body: Nothing, status }
+    else do
+      promise <- liftEff (jsonImpl response)
+      b <- Promise.toAff promise
+      pure { body: Just b, status }
