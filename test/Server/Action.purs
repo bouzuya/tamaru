33cbc82aeb34229d158bafb936fa01434c9c86d8
bouzuya <m@bouzuya.net>
2019-07-23 21:53:15 +0900

    add Server.Action

---
+++ test/Server/Action.purs
@@ -0,0 +1,20 @@
+module Test.Server.Action
+  ( tests
+  ) where
+
+import Prelude
+
+import Server.Action as Action
+import Test.Unit (TestSuite)
+import Test.Unit as TestUnit
+import Test.Unit.Assert as Assert
+
+tests :: TestSuite
+tests = TestUnit.suite "Server.Action" do
+  TestUnit.test "Show Action" do
+    Assert.equal "GetIndex" (show Action.GetIndex)
+    Assert.equal "GetGroupList" (show Action.GetGroupList)
+    Assert.equal "GetGroup(1)" (show (Action.GetGroup "1"))
+    Assert.equal "GetGroupDataList(1)" (show (Action.GetGroupDataList "1"))
+    Assert.equal "UpdateGroupData(1)" (show (Action.UpdateGroupData "1"))
+    Assert.equal "GetGroupData(1,2)" (show (Action.GetGroupData "1" "2"))
